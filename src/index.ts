3605819eee48387abe1ce4542f4b5f94c96c2647
MelvDouc <melv.douc@gmail.com>
2023-12-25 11:16:48 +0100

    Fix .npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import type * as PGNify from "$src/typings/types.public.js";
+
 export { default as PGNParser } from "$src/Parser.js";
 export { default as GameResults } from "$src/constants/GameResults.js";
 export type { default as Variation } from "$src/Variation.js";
-export type * as PGNify from "$src/typings/types.public.js";
+export type { PGNify };
